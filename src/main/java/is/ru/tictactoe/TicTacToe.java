09a39eacab7e23b04d6231c5576c4993dfc9bb90
Hjalti Steinar <hjaltisteinar@clients-pool-1.nat.ru.is>
2016-10-27 17:02:38 +0000

    few hidden . files

---
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -0,0 +1,8 @@
+package is.ru.tictactoe;
+
+public class TicTacToe {
+	public static void main(String args[]) {
+		System.out.println("Tic Tac Toe");
+		System.exit(0);
+	}
+}
---
+++ src/test/java/is/ru/tictactoe/TicTacToeTests.java
@@ -0,0 +1,13 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TicTacToeTests {
+	
+	@Test
+	public void test(){
+		assertEquals("HelloWorld", "HelloWorld");
+	}
+
+}
