523f9bf85e6915a35ea7611e0e48ed8ce7311aea
sveppalicious <sverrir.sigurdarson@gmail.com>
2016-10-27 18:34:00 +0000

    Added greeting at execution

--- src/main/java/is/ru/tictactoe/ConsoleInterface.java
+++ src/main/java/is/ru/tictactoe/ConsoleInterface.java
@@ -1,7 +1,10 @@
-package is.ru.tictactoe;
 
+package is.ru.tictactoe;
 public class ConsoleInterface {
 	public static void main(String args[]) {
-		System.out.println("Hello!");
+		System.out.println("Welcome to Tic Tac Toe.");
+        System.out.println("Player 1 is X");
+        System.out.println("Player 2 is O");
+        System.out.println("Player 1 starts");
 	}
 }
