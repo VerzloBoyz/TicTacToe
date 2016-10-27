9969a63465e58d962f46ec9ade6f27b313b590d2
armannari <armann15@ru.is>
2016-10-27 19:52:55 +0000

    Setting up contructor and connecting to business logic

--- src/main/java/is/ru/tictactoe/ConsoleInterface.java
+++ src/main/java/is/ru/tictactoe/ConsoleInterface.java
@@ -1,10 +1,23 @@
-
 package is.ru.tictactoe;
+
 public class ConsoleInterface {
-	public static void main(String args[]) {
+
+	private TicTacToe game;
+
+	ConsoleInterface(){
+
+		game = new TicTacToe();
+
 		System.out.println("Welcome to Tic Tac Toe.");
         System.out.println("Player 1 is X");
         System.out.println("Player 2 is O");
         System.out.println("Player 1 starts");
+	}
+
+	public static void main(String args[]) {
+		
+        ConsoleInterface interf = new ConsoleInterface();
+        
+
 	}
 }
