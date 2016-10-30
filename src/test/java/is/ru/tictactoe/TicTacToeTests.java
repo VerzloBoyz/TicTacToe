9eb35d3c57c3eac5ef97132220b1d08933f9f581
sveppalicious <sverrir.sigurdarson@gmail.com>
2016-10-30 23:59:31 +0000

    tests to dev

--- src/test/java/is/ru/tictactoe/TicTacToeTests.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTests.java
@@ -90,5 +90,70 @@ public class TicTacToeTests {
         assertEquals('X', testClass.checkWinner());
     }
 
+    @Test
+    public void testGetPlayer() {
+        TicTacToe testClass = new TicTacToe();
+        assertEquals("Player 1", testClass.getPlayer());
+        testClass.insertAt(5);
+        assertEquals("Player 2", testClass.getPlayer());
+    }
 
+    @Test
+    public void testReset() {
+        TicTacToe testClass = new TicTacToe();
+        assertEquals(true, testClass.reset());
+    }
+
+    // @Test
+    // public void testCheckLegalInsert() {
+    //   TicTacToe testClass = new TicTacToe();
+    //   testClass.insertAt(5);
+    //   assertEquals(false, testClass.checkLegalInsert(5));
+    // }
+
+    @Test
+    public void testNobodyWins() {
+      TicTacToe testClass = new TicTacToe();
+      char NOBODY_HAS_WON = '-';
+      char DRAW = '+';
+      testClass.insertAt(0); // X
+      testClass.insertAt(3); // O
+      testClass.insertAt(6); // X
+      testClass.insertAt(1); // O
+      assertEquals(NOBODY_HAS_WON, testClass.checkWinner());
+      testClass.insertAt(7); // X
+      assertEquals(NOBODY_HAS_WON, testClass.checkWinner());
+      testClass.insertAt(4); // O
+      testClass.insertAt(2); // X
+      testClass.insertAt(8); // O
+      testClass.insertAt(5); // X
+      assertEquals('+', testClass.checkWinner());
+    }
+
+    @Test
+    public void testWinnerXSecondCol() {
+      TicTacToe testClass = new TicTacToe();
+
+      testClass.insertAt(1); // X
+      testClass.insertAt(0); // O
+      testClass.insertAt(4); // X
+      testClass.insertAt(3); // O
+      testClass.insertAt(7); // X
+
+      assertEquals('X', testClass.checkWinner());
+    }
+
+    @Test
+    public void testWinnerXThirdCol() {
+
+      TicTacToe testClass = new TicTacToe();
+
+      testClass.insertAt(2); // X
+      testClass.insertAt(0); // O
+      testClass.insertAt(5); // X
+      testClass.insertAt(3); // O
+      testClass.insertAt(8); // X
+
+      assertEquals('X', testClass.checkWinner());
+    }
 }
